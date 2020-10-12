b1f089186ed00f5bdb19bc13de72955c417b3b4e
Domingo Cabeza <domingoacd1@gmail.com>
2020-10-12 16:10:40 -0400

    Added express

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   const navLinks = document.querySelectorAll('.j-nav-element');
   const allViews = document.querySelectorAll('.j-screen');
   
-  let currentView = 1;
+  let currentView = 0;
   let currentTranslation = 0;
 
   const translateScreensForwards = () => {
@@ -12,12 +12,24 @@
   }
     
 
-    const translateScreensBackwards = () => {
-      currentTranslation = currentTranslation + 100;
-      currentView--;
-      allViews.forEach(view => view.style.transform = `translateX(${currentTranslation}%)`);
-    } 
+  const translateScreensBackwards = () => {
+    currentTranslation = currentTranslation + 100;
+    currentView--;
+    allViews.forEach(view => view.style.transform = `translateX(${currentTranslation}%)`);
+  } 
       
+
+  function changeUrl(urlId) {
+    console.log(history);
+    history.pushState(
+      {
+        currentPageId: urlId
+    
+      },
+      'Domingo ' + urlId,
+      '/test'+ urlId
+    );
+  }
 
   function changeScreen(e) {
     e.preventDefault();
@@ -25,14 +37,18 @@
     
     const clickedLinkNumber = Number(this.dataset.link);
 
-    while(clickedLinkNumber !== currentView) {
-      clickedLinkNumber > currentView
-        ? translateScreensForwards()
-        : translateScreensBackwards();
+    if (clickedLinkNumber !== currentView) {
+      changeUrl(clickedLinkNumber);
     }
+      while (clickedLinkNumber !== currentView) {
+        clickedLinkNumber > currentView
+          ? translateScreensForwards()
+          : translateScreensBackwards();
+      }
+    
 
   }
 
   navLinks.forEach(link => link.addEventListener('click', changeScreen));
-
+  window.onpopstate = (e) => console.log('pop');
 }())
